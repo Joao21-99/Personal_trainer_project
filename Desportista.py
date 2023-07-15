5c576cc7813a2c33788c6bfa769286ba661ae683
João Yanga <joao.yanga@gmail.com>
2023-07-15 17:17:19 +0100

    improve the function to change data

--- Desportista.py
+++ Desportista.py
@@ -16,7 +16,7 @@ def novo():
         nivel = input('Nível: ')
         limit = input('Limitações: ')
 
-        cliente = {'Nome': nome, 'Nº telefone': num_tel, 'Morada': morada, 'Cartão de cidadão':cc, 'Nível': nivel, 'Limitações': limit}
+        cliente = {'NOME': nome, 'TELEMÓVEL': num_tel, 'MORADA': morada, 'CARTÃO DE CIDADÃO':cc,'DATA DE NASCIMENTO': data_nasc, 'NÍVEL': nivel, 'LIMITAÇÕES': limit}
         lista_clientes.append(cliente)
         resp = str(input('Pretende continuar? (S/N)')).upper().strip()
         if resp == 'N':
@@ -24,14 +24,27 @@ def novo():
     return lista_clientes
 
 def modificar(lst):
+    #questao == 'S'
     while True:
         nome = (input('Selecione o nome do desportista: '))
         for desp in lst:
             for k,v in desp.items():
                 if nome == v:
-                    resp = input(f'Olá {v}, em breve conseguiremos alterar os seus dados.')
-        if nome not in desp:
-            print('Atleta não encontrado.\nCertifique-se que escreveu o nome corretamente.')
+                        resp = input(f'Nome\n-Nº telemovel:\n-Morada\n-Cartão de cidadão\n- Data de nascimento\nNivel\nLimitações\nOlá {v}, que informação pretende alterar? ')
+                        
+                        if resp in desp:
+                                altera = input(f'{resp}: ')
+                                desp.update({resp: altera}) 
+                                print(f'\nAlteração bem sucedida!\n{desp}')
+                                break 
+                                
+                        else:
+                            print('Reposta inválida')
+                            break
+                            
+
+            if nome != v:
+                print('Atleta não encontrado.\nCertifique-se que escreveu o nome corretamente (Nome e Apelido).')
         questao = input('Pretende continuar? [S/N]').upper().strip()
         if questao == 'N':
             break
